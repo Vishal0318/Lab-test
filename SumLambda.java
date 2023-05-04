312d2ec1fd85cc816bbabc17b4878bce8b16deeb
Vishal0318 <rajput0318@gmail.com>
2023-05-05 00:15:16 +0530

    done

---
+++ SumLambda.java
@@ -0,0 +1,24 @@
+package Lambda_test;
+//marker interface  - empty interface 
+    interface xyz{}
+    
+//functional interface having only one method 
+
+interface calculation
+{
+	int sum(int a,int b);
+}
+
+public class SumLambda {
+
+	public static void main(String[] args) {
+//we are creating method with body but without method name 
+//this is lambda expression i.e shortcut way of creating a method 		
+		calculation c = (a,b)-> a+b;
+		
+		System.out.println(c.sum(10,20));
+		System.out.println(c.sum(22,33));
+		
+	}
+
+}
